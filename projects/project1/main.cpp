8b02c990b03bc6efcdc251e364934b22272f0525
Michael Maguire <maguiremq@gmail.com>
2026-05-18 19:19:02 -0400

    commit project 1 program

---
+++ projects/project1/main.cpp
@@ -0,0 +1,85 @@
+/*
+
+--------------------------------------------------------------------------------
+
+Author: Michael Maguire
+Course: COP 2000 - Introduction to Programming
+Institution: Santa Fe College
+Term: Summer 2026 (May 2026 - June 2026)
+Project: Project 1
+Program Name: main.cpp
+
+--------------------------------------------------------------------------------
+
+Project Specifications
+
+In this project, you will use Visual Studio to output the following information 
+on 4 lines of output only.  This means no additional blank lines between each 
+line of output.
+
+Display:
+
+1. your name on one line (please make this accurate)
+2. your address on one line (feel free to make up your address if you like), this 
+3. includes street address, city, state, zip
+4. your telephone number on one line (free free to make up your telephone 
+   number if you like), follow proper formatting
+5. your college major on one line (please make this accurate)
+
+Although these items should be displayed on separate output lines, 
+use only a single cout statement in your program.
+
+Be sure to:
+
+- only use one cout statement to display the output
+
+- only use 4 lines of output
+
+- use proper capitalization and spelling
+
+- use proper punctuation in the address and proper phone number formatting as
+  shown above add several blank lines at the very bottom of your program to move 
+  the output from Visual Studio away from your output.  These will not count 
+  toward the total number of lines used.
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+   cout
+      // Display Requirement 1
+      << "Michael Maguire\n"
+      // Display Requirement 2
+      << "100 SAS Campus Drive Cary, NC, 27513\n"
+      // Display Requirement 3
+      << "352-111-1111\n"
+      // Display Requirement 4
+      << "Computer Programming and Analysis"
+      // Requirement 5 - only one `cout` statement
+      << endl
+      << endl
+      << endl
+      << endl
+   ;
+
+   // End program
+   return 0;
+
+}
+
+/* 
+
+   Verification from log:
+
+   Michael Maguire
+   100 SAS Campus Drive Cary, NC, 27513
+   352-111-1111
+   Computer Programming and Analysis
+
+
+
+*/
