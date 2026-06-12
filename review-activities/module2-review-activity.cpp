4a4bad2f8d69e6ec85eda0f67666030def120dcc
Michael Maguire <maguiremq@gmail.com>
2026-06-12 08:03:55 -0400

    portion of mod2 review activity

---
+++ review-activities/module2-review-activity.cpp
@@ -0,0 +1,135 @@
+
+/*
+
+1. What is the ending value of y? pow(u,v) returns u raised to the power of v
+
+x = 2.0;
+y = 3.0;
+y = pow(pow(x, y)) + 1.0;
+
+8.0
+9.0
+Error the compiler complains about calling a function within another function
+65.0
+
+2. Which is a correct scientific notation for the floating-point literal: 0.00001
+
+0.1e-5
+1.0e-5
+1.0e-6
+0.1e-6
+
+3. Which expression fails to compute the area of a triangle having base b and height h (area is one-half base time height)?
+
+x = (1 / 2) * b * h;
+x = (b * h) / 2.0;
+x = 0.5 * b * h;
+x = (1.0 / 2.0) * b * h;
+
+4. What is the ending value of w?
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+     int w;
+     int y;
+
+     y = 34;
+
+     w = y % 5;
+
+     cout << "w is "
+          << w
+     ;
+
+     return 0;
+
+}
+
+4
+
+5. An identifier can:
+
+contain a period
+be a reserved word
+contain spaces
+start with an underscore
+
+start with an underscore
+
+6. What is the ending value of z
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+     int x;
+     int y;
+     int z; 
+
+     x = 0;
+     y = 3;
+     z = pow(x + 2, y);
+
+     cout << "z is "
+          << z
+     ;
+
+     return 0;
+
+}
+
+8
+
+7. 
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+     int numApples = 3;
+     double cost;
+
+     cost = 1.2 * numApples;
+
+     cout << "cost is "
+          << cost
+     ;
+
+     return 0;
+
+}
+     3.6
+
+8. 
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+     int x;
+     int y;
+
+     x = 2;
+     y = ((x + 1) / 5) + (x / 2);
+
+     cout << "y is "
+          << y
+     ;
+
+     return 0;
+
+}     
+
+1
+
+9. What is y after executing the statements
+
